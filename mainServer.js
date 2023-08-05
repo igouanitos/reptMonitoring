27f56e23e243b53e60671e54dace8921f25cb65e
igouanitos <siamourelis@gmail.com>
2023-08-05 23:46:34 +0300

    quite the progress
    signup // works
    login + authentication // works
    overview // works

--- mainServer.js
+++ mainServer.js
@@ -5,11 +5,23 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://127.0.0.1/reptMonitoring");
+mongoose.connect("mongodb://127.0.0.1/reptMonitoring", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+const userSchema = new mongoose.Schema({
+    userName : String,
+    userPw : String,
+    userEmail : String
+});
+
+const User = mongoose.model("User", userSchema);
 
 const animalSchema = new mongoose.Schema({
     user : String,
@@ -24,9 +36,66 @@ const animalSchema = new mongoose.Schema({
 
 const Animal = mongoose.model("Animal", animalSchema);
 
+var currentUser = {} ;
+
+//signup seems working
+//needs restrictions about the inputs, but its working
+app.post(`/signup`, (req,res)=>{
+
+    async function newUser(){
+        const user = new User({
+            userName : req.body.userName,
+            userPw : req.body.psw,
+            userEmail : req.body.email
+        });
+        user.save();
+        console.log(`user "${user.userName}" signed up successfully.`);
+        res.render(`login`,{pageName : 'Login'})
+    }
+
+    if (req.body.psw === req.body.psw_repeat){
+        newUser();
+    } else {
+        res.render(`signup`,{pageName : 'signup'});
+        console.log(`Passwords doesnt match.`);
+    }
+});
+
+app.get(`/signup`, (req,res)=>{
+    res.render(`signup`,{pageName : 'signup'});
+});
+
 app.get(`/login`, (req,res)=>{
     res.render(`login` ,{pageName : 'Login'});
 })
+
+app.post(`/login`, (req,res)=>{
+    const userNameTry = req.body.userName;
+    const userPwTry = req.body.psw;
+    async function userAuthenticator(){
+        try{
+            const query = {userName : userNameTry};
+            const user = await User.find(query);
+            if (user[0].userPw === userPwTry){
+                currentUser = user[0];
+                console.log(`user "${currentUser.userName}" successfully connected`);
+                res.render(`loginStatus` ,{pageName : 'success'});
+            } else{
+                console.log(`wrong password`);
+                res.render(`login` ,{pageName : 'wrong password'});
+            }
+
+
+        }catch(err){
+            console.log(`user "${userNameTry}" does not exist.`);
+            res.render(`login` ,{pageName : `user "${userNameTry}" does not exist.`});
+        }
+
+    }
+    userAuthenticator();
+
+});
+
 
 app.get('/',(req,res)=>{
     pageName = `home`;
@@ -47,7 +116,7 @@ app.post(`/register`, (req,res)=>{
     async function newAnimalRegister(){
 
         const animal = new Animal({
-            // user : userName, //something to link the user and the animal
+            user :currentUser._id,
             ssp : req.body.ssp,
             gender : req.body.gender,
             morph : req.body.morph,
@@ -63,8 +132,22 @@ app.post(`/register`, (req,res)=>{
     res.redirect(`/register`);
 })
 
+app.get(`/overview`, (req,res)=>{
 
+    async function userLivestock (){
+        try{
+            const find = {user : currentUser._id};
+            const findResult = await Animal.find(find);
+            res.render(`overview`, {pageName : `overview`, content : findResult });
+        }catch(err){
+            console.error(err);
+            res.render(`overview`, {pageName : `error`, content : `error` });
+        }
+    }
 
+    userLivestock();
+
+})
 
 
 app.listen(port, ()=>{
